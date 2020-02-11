8a2dd51ad7b26c2bee6133102dea2d358d7546ce
GiedriusN1 <50556207+GiedriusN1@users.noreply.github.com>
2020-02-11 17:56:14 +0200

    if then else / if then else if
    if then else / if then else if

---
+++ IfThenElse.java
@@ -0,0 +1,32 @@
+public class IfThenElse
+{
+   public static void main(String[] args)
+   {
+	   
+	   int a;
+	   int b;
+	   char result = '>';
+	   
+	   System.out.println("Iveskite a");
+	   a = Integer.valueOf(System.console().readLine());
+	   System.out.println("Iveskite b");
+	   b = Integer.valueOf(System.console().readLine());
+	   
+	   if (a > b) {
+		   result = '>';
+	   }
+	   
+	   if (a < b) {
+           result = '<';
+       }
+
+       if (a ==	b) {
+           result = '=';
+       }
+
+//    System.out.println("10");
+      System.out.println("a " + result + " b");
+//    System.out.println(result);
+
+   }
+}   
